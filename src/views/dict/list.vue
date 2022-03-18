3d23ae70afb0132d1200ced69f29b1735bab5e6d
soap-cpp <995560375@qq.com>
2022-03-18 14:26:00 +0800

    数据字典导入导出

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -1,5 +1,9 @@
 <template>
+    
     <div class="app-container">
+        <el-button type="success" @click="exportData">导出字典数据到本地</el-button>
+        <el-button type="success" @click="importData">导入本地数据到字典</el-button>
+        
         <el-table
         :data="list"
         style="width: 100%"
@@ -30,6 +34,28 @@
                 </template>
             </el-table-column>
         </el-table>
+        <!-- 导入数据 -->
+        <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+        <el-form label-position="right" label-width="170px">
+
+        <el-form-item label="文件">
+        <el-upload
+        :multiple="false"
+        :on-success="onUploadSuccess"
+        :action="'http://localhost:8202/admin/cmn/dict/importData'"
+        class="upload-demo">
+        <el-button size="small" type="primary">点击上传</el-button>
+        <div slot="tip" class="el-upload__tip">只能上传xls文件，且不超过500kb</div>
+        </el-upload>
+        </el-form-item>
+
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">
+            取消
+        </el-button>
+        </div>
+        </el-dialog>
     </div>  
 </template>
 <script>
@@ -37,7 +63,8 @@ import dict from "@/api/dict"
 export default {
     data(){
         return {
-            list: []
+            list: [],
+            dialogImportVisible: false
         }
     },
     created(){
@@ -55,6 +82,17 @@ export default {
                 .then(res => {
                     resolve(res.data)
                 })
+        },
+        exportData(){
+            console.log("导出数据到本地---------")
+            window.location.href = "http://localhost:8202/admin/cmn/dict/exportData"
+        },
+        importData(){
+            this.dialogImportVisible = true;
+        },
+        onUploadSuccess(){
+            this.dialogImportVisible = false;
+            this.getDictList(1);
         }
     }
 }
